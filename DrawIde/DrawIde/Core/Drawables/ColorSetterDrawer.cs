967a0205f3f6bcffd88b4e11e70115b34d72f305
Vasile Pojoga <vasea.pojoga@gmail.com>
2017-05-05 12:36:12 +0300

    Implemented color setter drawer

---
+++ DrawIde/DrawIde/Core/Drawables/ColorSetterDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DrawIde.Core.Drawables
+{
+    class ColorSetterDrawer : IDrawable
+    {
+        private readonly string color;
+
+        public ColorSetterDrawer(string color)
+        {
+            this.color = color;
+        }
+
+        public void Draw(IDrawingContext context)
+        {
+            context.Color = this.color;
+        }
+    }
+}
