0b2fe103482160d8931fdc4ee86e5f6a2c08347b
vasica38 <vasica38@gmail.com>
2017-05-05 13:23:16 +0300

    Added Rectangle

---
+++ DrawIde/DrawIde/Core/Drawables/RectangleDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawIde.Core.Drawables
+{
+    class RectangleDrawer : IDrawable
+    {
+        int x, y;
+        int x1, y1;
+
+        public RectangleDrawer(int x, int y, int x1, int y1)
+        {
+            this.x = x;
+            this.y = y;
+            this.x1 = x1;
+            this.y1 = y1;
+        }
+
+        public void Draw(IDrawingContext context)
+        {
+            var graphics = context.Graphics;
+            Pen pen = new Pen(System.Drawing.Color.FromName(context.Color));
+            graphics.DrawRectangle(pen, new Rectangle(x, y, x1, y1));
+        }
+    }
+}
